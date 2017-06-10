9c892ff4d59a850b25cb254f4c254117f0e60472
artlenk.ru@gmail.com <df5ukoi7ht>
2017-06-10 13:02:40 +0300

    Initial commit

---
+++ src/js/common.js
@@ -0,0 +1,8 @@
+$('.single-item').slick();
+
+$('body').blurjs({
+    source: 'body',
+    radius: 7,
+    overlay: 'rgba(255,255,255,0.4)'
+});
+
---
+++ src/js/lib.js
@@ -0,0 +1,10 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+//= ../../bower_components/slick-carousel/slick/slick.js
+
+//= ../../bower_components/Blur.js/dist/jquery.stack-box-blur.js
+
+
+// = lib/smoothscroll.js
+// = lib/jquery.fancybox.min.js
+// = lib/svgxuse.min.js
