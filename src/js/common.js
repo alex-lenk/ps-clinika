0d60f76eb1b01a301941f9cad8ec192f3838fea9
artlenk.ru@gmail.com <df5ukoi7ht>
2017-06-12 12:32:18 +0300

    Create mobile menu

--- src/js/common.js
+++ src/js/common.js
@@ -4,4 +4,35 @@ $(document).ready(function () {
         arrows: false,
         fade: true
     });
+
+    $(".navbar-toggle").click(
+        function () {
+            $('.menu').toggleClass("menu-active");
+        }
+    );
 });
+
+var navPos, winPos, navHeight, nav = $(".nav-menu");
+
+function refreshVar() {
+    navPos = nav.offset().top;
+    navHeight = nav.outerHeight(true);
+}
+
+refreshVar();
+$(window).resize(refreshVar);
+
+$('<div class="clone-nav"></div>').insertBefore(".nav-left").css("height", navHeight).hide();
+
+$(window).scroll(function () {
+    winPos = $(window).scrollTop();
+
+    if (winPos >= navPos) {
+        nav.addClass("fixed shadow");
+        $(".clone-nav").show();
+    } else {
+        nav.removeClass("fixed shadow");
+        $(".clone-nav").hide();
+    }
+});
+
